ec1ef56dfa35a5c366ca0660fdb3bf7f13fb0767
fdeitch <faustoluzdeitch@gmail.com>
2023-01-25 15:15:03 -0300

    adcionando

--- conta.py
+++ conta.py
@@ -1,15 +1,26 @@
 class Conta:
     def __init__(self,numero,titular,saldo,limite):
         print(f'Construindo objeto...{self}')
-        self.numero=numero
-        self.titular=titular
-        self.saldo=saldo
-        self.limite=limite
+        self.__numero=numero
+        self.__titular=titular
+        self.__saldo=saldo
+        self.__limite=limite
     def extrato(self):
-        print(f'Saldo {self.saldo} do titular {self.titular}')
+        print(f'Saldo {self.__saldo} do titular {self.__titular}')
     def deposita(self,valor):
-        self.saldo+=valor
+        self.__saldo+=valor
     def saca(self,valor):
-        self.saldo-=valor
-        
-
+        self.__saldo-=valor
+    def transfere(self,valor,destino):
+        self.saca(valor)
+        destino.deposita(valor)
+    def get_saldo(self):
+        return self.__saldo
+    def get_titular(self):
+        return self.__titular
+    @property
+    def limite(self):
+        return self.__limite
+    @limite.setter
+    def limite(self,limite):
+        self.__limite=limite
