05748ec59d8bb2e2ed7cefcafba061babfd387a9
fdeitch <faustoluzdeitch@gmail.com>
2023-01-25 18:09:36 -0300

    finalizando

--- conta.py
+++ conta.py
@@ -1,6 +1,6 @@
 class Conta:
     def __init__(self,numero,titular,saldo,limite):
-        print(f'Construindo objeto...{self}')
+        #print(f'Construindo objeto...{self}')
         self.__numero=numero
         self.__titular=titular
         self.__saldo=saldo
@@ -9,14 +9,22 @@ class Conta:
         print(f'Saldo {self.__saldo} do titular {self.__titular}')
     def deposita(self,valor):
         self.__saldo+=valor
+    def __pode_sacar(self,valor_a_sacar):
+        valor_disponivel_a_sacar=self.__saldo+self.__limite
+        return valor_a_sacar<=valor_disponivel_a_sacar
     def saca(self,valor):
-        self.__saldo-=valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo-=valor
+        else:
+            print(f'O valor {valor} passou o limite.')
     def transfere(self,valor,destino):
         self.saca(valor)
         destino.deposita(valor)
-    def get_saldo(self):
+    @property
+    def saldo(self):
         return self.__saldo
-    def get_titular(self):
+    @property
+    def titular(self):
         return self.__titular
     @property
     def limite(self):
@@ -24,3 +32,11 @@ class Conta:
     @limite.setter
     def limite(self,limite):
         self.__limite=limite
+    @staticmethod
+    def codigo_banco():
+        return '001'
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001','Caixa':'104','Bradesco':'237'}
+from conta import Conta 
+print(Conta.codigo_banco())       
