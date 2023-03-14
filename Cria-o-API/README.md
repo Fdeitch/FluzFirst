68d75dc64cf37daabbb614c4a40623c8d6a62d05
Fausto <faustoluzdeitch@gmail.com>
2023-03-14 19:14:56 -0300

    Cria-o-api projeto adicionado

---
+++ Cria-o-API/README.md
@@ -0,0 +1,1 @@
+# Cria-o-API
---
+++ Cria-o-API/escola/views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+from escola.models import Aluno,Curso
+from serializer import AlunoSerializer,CursoSerializer
+class AlunosViewSet(viewsets.ModelViewSet):
+    '''Exibindo todos os alunos e alunas'''
+    queryset=Aluno.objects.all()
+    serializer_class=AlunoSerializer
+class CursosViewSet(viewsets.ModelViewSet):
+    '''Exibindo todos os cursos'''
+    queryset=Curso.objects.all()
+    serializer_class=CursoSerializer
